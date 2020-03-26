cf3ad8fa2b8e31d8667e700db32c98bde88091a8
Gregory Hapgood <greghap@gmail.com>
2020-03-26 16:27:03 -0500

    Making CMakeLists.txt

--- compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%cd:~0,1%:\PortableSoftware\MinGW\bin;%PATH%
-g++ -Wall -Wextra -Werror -Wpedantic -std=c++1z Snake.cpp Main.cpp -o Snake -I E:\PortableSoftware\MinGW\external\include -L E:\PortableSoftware\MinGW\external\lib\static -lpdcurses
